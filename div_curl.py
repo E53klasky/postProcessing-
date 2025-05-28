6c03cae9012175714247b46327fab596758ce801
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-05-28 13:04:35 -0400

    getting startedx

---
+++ div_curl.py
@@ -0,0 +1,14 @@
+from mpi4py import MPI
+import numpy as np 
+from adios2 import stream 
+
+
+def main():
+    # Initialize MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    print(f"Rank {rank} of {size} started.")
+    
+if __name__ == "__main__":
+    main()
