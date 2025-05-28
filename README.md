5e1007c590c4d8e0e4cb895014004bfa457c9f64
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-05-28 13:39:00 -0400

    xml fixed check Cavity adios xml for an example

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # postProcessing-
 This is where i will be doing my post processing of the cfd simulation of xcompact3d
+
+
+
+Add edits to adios2 xml config
+
+
+
+
+
+How to run
+
+div_curl 
+mpirun -np 4 python3 div_curl.py ../Incompact3d/examples/Cavity/data.bp5/ ../Incompact3d/examples/Cavity/adios2_config.xml
--- div_curl.py
+++ div_curl.py
@@ -1,14 +1,28 @@
 from mpi4py import MPI
 import numpy as np 
-from adios2 import stream 
-
+from adios2 import Adios, Stream
+import os
+import sys
 
 def main():
-    # Initialize MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
     print(f"Rank {rank} of {size} started.")
+    
+    
+    if(len(sys.argv) < 2):
+        print("Usage: python div_curl.py <PATH/TO/ADIOS2/DIR> <PATH/TO/ADIOS2/xml> ")
+        return
+    adios2_dir = sys.argv[1]
+    adios2_xml = sys.argv[2]
+    print(f"ADIOS2 directory: {adios2_dir}")
+    print(f"ADIOS2 XML file: {adios2_xml}")
+    
+    adios_obj = Adios(adios2_xml)
+    Rio = adios_obj.declare_io("readerIO")
+    Wio = adios_obj.declare_io("WriteIO")
+    
     
     
     
