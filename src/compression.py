f123a6a5eadcb878df9d3ad0b1e0b52d4bad1e56
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-07-08 14:24:55 -0400

    cleaning it

--- src/compression.py
+++ src/compression.py
@@ -102,7 +102,6 @@ def main():
             data = np.array(r.read_step(name), dtype=np.float64)
             w.write(name, data)
 
-
             write_start = time.time()
             w.write(name, data)
             if flag:
