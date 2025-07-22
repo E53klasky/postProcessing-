d133b09cafec9aa26d62c83ae5f18ce9093d9aea
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-07-22 14:54:16 -0400

    good

--- WorkFlows/second_workflow.sh
+++ WorkFlows/second_workflow.sh
@@ -2,7 +2,7 @@
 
 
 
-
+# gen data  
 mpirun -np 6 ../../build/bin/xcompact3d  input_ILES_Re5000.i3d
 
 # work flow 1
@@ -17,3 +17,11 @@ python3 createStreamlines.py -in ../../Incompact3d/examples/TGV-Taylor-Green-vor
 python3 errorStream3D.py  --file1 seg_compressed/ --file2 seg/ --var_x coords_x --var_y coords_y  --var_z coords_z  --var_offset offsets
 
 # work flow 2
+
+python3 prep_desimateter.py -in ../../Incompact3d/examples/TGV-Taylor-Green-vortex/data.bp5/ -v ux,uy,uz,critq,vort --method upsample
+
+python3 Decimation.py -in resampled.bp/ -v ux,uy,uz,vort,critq  -l 2
+
+python3 createStreamlines.py -in Decimated.bp/ -v ux,uy,uz  -o seg_decimated -s '(0.75,0.85,0.3)'
+
+python3 errorStream3D.py  --file1 seg_decimated/ --file2 seg/ --var_x coords_x --var_y coords_y  --var_z coords_z  --var_offset offsets
